416db1a7a9632b77344d5ab053f4622fc787886c
Sphax <itssphax123@gmail.com>
2024-05-30 20:03:51 +0000

    0. Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+"""Pascal triangle"""
 
+def pascal_triangle(n):
+    res = [[1]]
+
+    for i in range(n - 1):
+        tmp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(tmp[j] + tmp[j + 1])
+        res.append(row)
+    return res
