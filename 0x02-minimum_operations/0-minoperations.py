b1352eb1926d374312b3c3914191b18e9d2efb4c
Sphax <itssphax123@gmail.com>
2024-06-13 18:53:46 -0700

    0. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+def minOperations(n):
+    if n <= 1:
+        return 0
+    operations = 0
+    factor = 2
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    return operations
