4d2143920120fdb99abe6a67b962dd3e668cecdf
Sphax <itssphax123@gmail.com>
2024-07-18 07:09:48 -0700

    0. Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rotate a 2D Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """Function"""
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
